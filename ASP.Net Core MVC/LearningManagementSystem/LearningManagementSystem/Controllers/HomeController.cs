0d37b8777f8ece1266769911ba0588cc0efcb98c
Sarapriya03 <sarapriya2912@gmail.com>
2025-06-15 19:33:31 +0530

    Added Case Study

---
+++ ASP.Net Core MVC/LearningManagementSystem/LearningManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningManagementSystem.Controllers
+{
+    [Route("home")]
+    public class HomeController : Controller
+    {
+        // Route: / or /home or /home/index
+        [HttpGet("")]
+        [HttpGet("index")]
+        [Route("/")] // maps root URL directly
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Route: /home/about
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // Route: /home/contact
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.Net Core MVC/LearningManagementSystem/LearningManagementSystem/Models/Content.cs
@@ -0,0 +1,12 @@
+﻿namespace LearningManagementSystem.Models
+{
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public string Title { get; set; }
+        public string FileUrl { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
