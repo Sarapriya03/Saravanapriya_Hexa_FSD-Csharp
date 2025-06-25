5abd0d34bdb62b428f34233ef58266401ce78e37
Sarapriya03 <sarapriya2912@gmail.com>
2025-06-25 16:47:11 +0530

    Added Microservices Case Study

---
+++ Microservices using Api Gateway/EMS/EMS/HttpClientApp/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using HttpClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientApp.Controllers
+{
+    [Route("Employee")]
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClientFactory = httpClientFactory;
+        }
+
+        public HttpClient GetClient()
+        {
+            var client = _httpClientFactory.CreateClient("ApiGatewayClient");
+            return client;
+        }
+
+        //GET:Retrieve all the departments from Department Microservice through API Gateway
+        [Route("/")]
+        public async Task<IActionResult> EmployeeList()
+        {
+            var client = GetClient();
+            var employees = await client.GetFromJsonAsync <List<Employee>>("gateway/employees");
+            return View(employees);
+        }
+
+    }
+}
---
+++ Microservices using Api Gateway/EMS/EMS/HttpClientApp/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpClientApp.Models
+{
+    public class Employee
+    {
+        public int EmpCode { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public int DeptCode { get; set; }
+    }
+}
